6b18e70e7f8b9a1503ae98f62b8dbdeb6b3eb166
Marc Ouwehand <145332849+MarcAOuwehand@users.noreply.github.com>
2024-03-17 12:45:34 +0100

    Update data-storage.service.ts

--- src/app/data-storage.service.ts
+++ src/app/data-storage.service.ts
@@ -6,7 +6,7 @@ import { OrderService } from "./cart/order/order.service";
 
 @Injectable()
 export class DataStorageService {
-  private apiUrl = 'https://s1142622-iprwc.store:8081/';
+  private apiUrl = 'https://s1142622-iprwc.store:8081';
 
   constructor(
     private http: HttpClient,
